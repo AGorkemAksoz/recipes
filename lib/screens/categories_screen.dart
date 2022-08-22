76e5f30d1f4d169188cf6e2c13be042c3be20c02
a.gorkem17 <a.gorkem17@gmail..com>
2022-08-22 15:14:49 +0300

    GridView has done

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:recipes/model/category_item.dart';
+
+import '../dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("DeliMe"),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map(
+              (catDATA) => CategoryItem(
+                title: catDATA.title,
+                color: catDATA.color,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
